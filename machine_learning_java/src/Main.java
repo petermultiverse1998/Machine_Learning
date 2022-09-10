2d11479b53378101a0b6451157f7af6c613f18ae
Ganesh Rai <53878675+petermultiverse1998@users.noreply.github.com>
2022-09-10 17:24:30 +0545

    update

--- machine_learning_java/src/Main.java
+++ machine_learning_java/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         montecarlo.Main.main(args);
 //        minimax.Main.main(args);
     }
--- machine_learning_java/src/montecarlo/Main.java
+++ machine_learning_java/src/montecarlo/Main.java
@@ -1,11 +1,15 @@
 package montecarlo;
 
+import neat.Genome;
+import neat.NEAT;
+import neat.NeatGraphics;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         MonteCarlo monteCarlo = new MonteCarlo(0,0,0,0);
         monteCarlo.setAvailableActions(currentEncodedState -> {
             if(currentEncodedState[3]==1)
@@ -45,5 +49,36 @@ public class Main {
         int bestAction = monteCarlo.bestAction(100);
         System.out.println(monteCarlo);
         System.out.println(bestAction);
+
+//        Neat neat = new Neat();
+//        NeatGraphics graphics = new NeatGraphics();
+//        Genome fittest = null;
+//        for (int i = 0; i < 100; i++) {
+//            neat.evolve();
+//            fittest = neat.fittestGenome();
+//            graphics.draw(fittest);
+//            Thread.sleep(10);
+//        }
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i+" : "+fittest.forward(1,i)[0]);
+//        }
+
+    }
+
+    static class Neat extends NEAT{
+        public Neat() {
+            super(1000, 2, 1, 10, true, false, true);
+        }
+
+        @Override
+        public float fitness(Genome genome) {
+            float error = 0;
+            for (int i = 0; i < 10; i++) {
+                float f = genome.forward(1,i)[0];
+                error += (3*i+1-f) *(3*i+1-f);
+            }
+            return -error;
+        }
     }
 }
